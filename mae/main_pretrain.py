8f33a9d827189938b5a9eb7215ba9900755e42f4
Emir98 <eevardar@hotmail.com>
2022-11-14 20:32:17 +0100

    For some reason the wandb part disappered. So put it back here.

--- mae/main_pretrain.py
+++ mae/main_pretrain.py
@@ -34,6 +34,7 @@ import models_mae
 
 from engine_pretrain import train_one_epoch
 
+import wandb
 
 def get_args_parser():
     parser = argparse.ArgumentParser('MAE pre-training', add_help=False)
@@ -118,6 +119,13 @@ def get_args_parser():
 
 def main(args):
     misc.init_distributed_mode(args)
+
+    # WandB init
+    wandb.init(
+        project="DL_advanced_mae",
+        config=args,
+        sync_tensorboard=True
+    )
 
     print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
     print("{}".format(args).replace(', ', ',\n'))
@@ -225,6 +233,9 @@ def main(args):
 
         log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
                         'epoch': epoch,}
+        
+        """wandb.log({**{f'train_{k}': v for k, v in train_stats.items()},
+                        'epoch': epoch,})"""
 
         if args.output_dir and misc.is_main_process():
             if log_writer is not None:
@@ -235,7 +246,7 @@ def main(args):
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
-
+    wandb.finish()
 
 if __name__ == '__main__':
     args = get_args_parser()
