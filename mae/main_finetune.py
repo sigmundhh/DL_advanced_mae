088c39b05921e351e2a61c3b614ff7fc7380e4e8
Vardar98 <77897641+Vardar98@users.noreply.github.com>
2022-11-17 17:48:22 +0100

    Update main_finetune.py

--- mae/main_finetune.py
+++ mae/main_finetune.py
@@ -345,7 +345,7 @@ def main(args):
             log_writer=log_writer,
             args=args
         )
-        if args.output_dir and (epoch % 10 == 0 or epoch + 1 == args.epochs) == 0:
+        if args.output_dir and (epoch % 10 == 0 or epoch + 1 == args.epochs):
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
                 loss_scaler=loss_scaler, epoch=epoch)
