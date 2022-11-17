08a6e9a40aaa1a494e6ebe3d2468fdc34028510f
Vardar98 <77897641+Vardar98@users.noreply.github.com>
2022-11-17 16:47:34 +0100

    Update main_finetune.py
    Save the model each 10 epoch

--- mae/main_finetune.py
+++ mae/main_finetune.py
@@ -345,7 +345,7 @@ def main(args):
             log_writer=log_writer,
             args=args
         )
-        if args.output_dir:
+        if args.output_dir and args.epochs%10 == 0:
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
                 loss_scaler=loss_scaler, epoch=epoch)
