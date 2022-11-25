f5c5757906d42bf77cddcbcc9579de972c41e86e
Vardar98 <77897641+Vardar98@users.noreply.github.com>
2022-11-25 17:34:35 +0100

    Update main_cnn_pretrain.py

--- mae/main_cnn_pretrain.py
+++ mae/main_cnn_pretrain.py
@@ -29,6 +29,8 @@ def get_args_parser():
                         help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
 
     # Model parameters
+    parser.add_argument('--model', default='cnn', type=str, metavar='MODEL',
+                        help='Name of model to train')
     parser.add_argument('--input_size', default=224, type=int,
                         help='images input size')
     parser.add_argument('--mask_ratio', default=0.75, type=float,
